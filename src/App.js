5a725d74e11c074149dfb830916f5915243bea0f
Kevin-Zagar <43926874+Kevin-Zagar@users.noreply.github.com>
2019-10-31 14:08:56 +0100

    Navbar

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import styled from 'styled-components';
+import Navbar from './components/layout/Navbar';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const BodyDiv = styled.div`
+  box-sizing: border-box;
+  padding: 0;
+  margin: 0;
+  font-family:'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+  line-height: 1.7;
+`
+const Header1 = styled.h1 {
+  
 }
 
-export default App;
+export class App extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default App
---
+++ src/components/layout/Navbar.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import styled from 'styled-components';
+
+const Nav = styled.nav`
+background-color: 
+`
+
+export class Navbar extends Component {
+    render() {
+        return (
+            <Nav className='navbar'>
+                <h1>Hello</h1>
+            </Nav>
+        )
+    }
+}
+
+export default Navbar;
